dde08ed9d0491c79579196a245069ae3c9636e6b
Jahid101 <jahidhasananik.official@gmail.com>
2021-06-03 16:49:14 +0600

    lets start

--- App.js
+++ App.js
@@ -1,6 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as firebase from 'firebase'
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBoRzAE-Jxy3S4uqQAqlhLxYm4PSw4znYw",
+  authDomain: "react-native-chat-a1787.firebaseapp.com",
+  projectId: "react-native-chat-a1787",
+  storageBucket: "react-native-chat-a1787.appspot.com",
+  messagingSenderId: "837362959957",
+  appId: "1:837362959957:web:cfd825c399c8140d8db6dc"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 export default function App() {
   return (
